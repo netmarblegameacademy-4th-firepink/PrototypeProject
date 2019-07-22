04498559d14c463f52bb22444c967cfa80fc38c1
SoonHyung_Lee <tnsgud9@naver.com>
2019-07-23 02:14:40 +0900

    Resource Upload and Scene Setting

---
+++ Assets/#Scripts/GunCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class GunCtrl : MonoBehaviour
+{
+
+    
+    public GameObject laserPrefab;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.Joystick1Button9) || Input.GetKey(9));
+         //   Debug.Log("작동");
+    }
+
+
+}
