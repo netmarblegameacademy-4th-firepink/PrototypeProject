dfa2dcba42cbee919a33bdb896a2e28a665a6a86
SoonHyung_Lee <tnsgud9@naver.com>
2019-07-23 03:22:28 +0900

    SteamVR Version Downgrade( now 1.2.3) & enemy Resource Add

--- Assets/#Scripts/GunCtrl.cs
+++ Assets/#Scripts/GunCtrl.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Valve.VR;
 public class GunCtrl : MonoBehaviour
 {
-
-    
+    public Transform firePoint;
     public GameObject laserPrefab;
+
+    SteamVR_TrackedObject controller;
+    SteamVR_Controller.Device device;
 
     
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        controller = GetComponent<SteamVR_TrackedObject>();
 
     }
 
+    // public SteamVR_Action_Boolean spawn = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.Joystick1Button9) || Input.GetKey(9));
-         //   Debug.Log("작동");
+        device = SteamVR_Controller.Input((int)controller.index);
+
+        if(device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("work!");
+        }
+
     }
 
 
